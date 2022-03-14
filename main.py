04588973116e18e0907e5c4b031b48a0c3cde19e
Rea Tabofunda <reacarolyn@gmail.com>
2022-03-14 23:16:51 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,22 @@
+#100 Days of Code Project (Day1 Python)
+
+#print function
+print("Hello" + "Rea")
+
+#input & length method
+print(len(input('What is your name? ')))
+
+#variables
+name = input('What is your lastname')
+print(name)
+
+# switch variables
+a = input("a:")
+b = input("b:")
+c = a
+a = b
+b = c
+print ("a:" + a)
+print ("b:" + b)
+
+
